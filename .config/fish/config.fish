509a318fe17dfdf6f6f7c627a37eacb3a68f4989
Matan Kushner <hello@matchai.me>
2019-01-13 16:47:28 -0500

    Replace z with jump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,14 @@ source ~/.config/fish/paths.fish
 # Source secret if it exists
 sourceadd ~/.config/fish/secret.fish
 
-# jabba
-sourceadd ~/.jabba/jabba.fish
+# Configure Jump
+status --is-interactive; and source (jump shell fish | psub)
 
 # Load all saved ssh keys
 /usr/bin/ssh-add -A ^/dev/null
+
+# Set the emoji width for iTerm
+set -g fish_emoji_width 2
 
 # Hide the fish greeting
 set fish_greeting ""
