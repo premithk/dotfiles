8e9bbe53b2e64c4dc724df0cef6607fddb7bbe4f
Matan Kushner <hello@matchai.me>
2019-02-18 00:07:26 -0500

    brew: replace wireguard-go with MAS app

--- .config/fish/functions/vpd.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function vpd --description 'alias vpd wg-quick down mullvad-ca3-ca1'
-	wg-quick down mullvadca3 $argv;
-end
--- .config/fish/functions/vpu.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function vpu --description 'alias vpu wg-quick up mullvad-ca3-ca1'
-	wg-quick up mullvadca3 $argv;
-end
