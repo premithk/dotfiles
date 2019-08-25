4b44481c6fe1648cb76d869e73fb37642d467332
Matan Kushner <hello@matchai.me>
2019-08-25 13:06:02 -0400

    fish: Remove spacefish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,10 +16,6 @@ set -g fish_emoji_width 2
 # Hide the fish greeting
 set fish_greeting ""
 
-# Spacefish config
-set -g SPACEFISH_CHAR_SYMBOL ❯
-set -g SPACEFISH_EXEC_TIME_ELAPSED 2
-
 # Use legacy fzf keybindings
 set -g FZF_LEGACY_KEYBINDINGS 1
 
